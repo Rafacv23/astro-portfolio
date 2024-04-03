4cb2d0e26c4897f68f7c39a5b504c1061aeeeda2
Rafa Canosa <rafacv23@gmail.com>
2024-04-03 13:31:49 +0200

    added site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
+  site: "https://rafa-canosa-portfolio.vercel.app",
   output: "hybrid",
   i18n: {
     defaultLocale: "es",
-    locales: ["es", "en"]
+    locales: ["es", "en"],
   },
-  adapter: vercel()
-});
+  adapter: vercel(),
+})

22419d59642f2d05b498fe02f303ff450bf6e634
Rafa Canosa <rafacv23@gmail.com>
2024-05-21 13:30:19 +0200

    changed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
-
+import { SITE_URL } from "./src/lib/constants"
 import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: "https://rafa-canosa-portfolio.vercel.app",
+  site: SITE_URL,
   output: "hybrid",
   i18n: {
     defaultLocale: "es",

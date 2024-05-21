eed7ce92562100019be36c214948a3f0584fe96e
Rafa Canosa <rafacv23@gmail.com>
2024-05-21 13:54:59 +0200

    add new title

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,8 @@
 export const SITE_URL: string = "https://www.rafacanosa.dev"
+export const SITE_NAME_ES: string =
+  "Rafa Canosa | Desarrollador de Aplicaciones Web Front-end | Portfolio"
+export const SITE_NAME_EN: string =
+  "Rafa Canosa | Front end web Developer | Portfolio"
 export const SITE_DESCRIPTION_ES: string =
   "Portfolio de Rafa Canosa, desarrollador de aplicaciones web Front-end. Explora mis proyectos y habilidades en desarrollo web."
 export const SITE_DESCRIPTION_EN: string =

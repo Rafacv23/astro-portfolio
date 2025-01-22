cb96ea1c1af890e8ec3d0d6d191e93966e9ee5ca
Rafa Canosa <rafacv23@gmail.com>
2025-01-22 19:09:21 +0100

    refactor tailwind configuration: extend color palette for improved theming options

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F1F5F9",
+        secondary: "#C8C8CC",
+        accent: "#5BE5CF",
+        outline: "#10211F",
+        card: "#18181B",
+      },
+    },
   },
   plugins: [require("rippleui")],
 }

f15cf6e1ae786da06a171aac10b1bbcad8453c79
Rafa Canosa <rafacv23@gmail.com>
2025-01-23 16:52:27 +0100

    refactor API: remove unused findProjectBySlug functions for English and Spanish

--- src/lib/api/en/findProjectBySlug.js
+++
@@ -1,5 +0,0 @@
-import projectsData from "../../data/en/projects.json"
-
-export const findProjectBySlug = (slug) => {
-  return projectsData.projects.find((project) => project.slug === slug)
-}
--- src/lib/api/es/findProjectBySlug.js
+++
@@ -1,5 +0,0 @@
-import projectsData from "../../data/es/projects.json"
-
-export const findProjectBySlug = (slug) => {
-  return projectsData.projects.find((project) => project.slug === slug)
-}

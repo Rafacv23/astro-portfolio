e70339d6bef8ed66d4e1895cfa1e512d979f6435
Rafa Canosa <rafacv23@gmail.com>
2025-01-23 16:53:41 +0100

    feat(types): add Project type definition for project data structure

---
+++ src/lib/types.ts
@@ -0,0 +1,15 @@
+type Project = {
+  id: number | string
+  name: string
+  slug: string
+  languages: string[]
+  year: number
+  madeAt: string
+  functionalities: string[]
+  img: string
+  important: boolean
+  mobile: boolean
+  description: string
+  live?: string
+  github: string
+}

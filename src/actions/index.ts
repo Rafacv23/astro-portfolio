d52de2f09d769db6d79b6c8c401e13ebf3ff167c
Rafa <rafacv23@gmail.com>
2025-02-05 12:16:42 +0100

    feat: add name field to contact form and send confirmation email to user

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -9,10 +9,11 @@ export const server = {
     handler: async (formData) => {
       // Extraer datos del formulario
       const email = formData.get("email") as string
+      const name = formData.get("name") as string
       const subject = formData.get("subject") as string
       const message = formData.get("message") as string
 
-      if (!email || !subject || !message) {
+      if (!email || !subject || !message || !name) {
         throw new ActionError({
           code: "BAD_REQUEST",
           message: "Todos los campos son requeridos.",
@@ -21,15 +22,30 @@ export const server = {
 
       const { data, error } = await resend.emails.send({
         from: "Rafa Canosa <noreply@rafacanosa.dev>",
-        to: [email], // Envía el correo a la dirección proporcionada
+        to: "rafacanosa@gmail.com",
         subject,
-        html: `<p>${message}</p>`,
+        html: `<h2>from: ${name}</h2><p>${message}</p>`,
       })
 
       if (error) {
         throw new ActionError({
           code: "BAD_REQUEST",
           message: error.message,
+        })
+      }
+
+      // now we send a confirmation email to the user
+      const confirmationEmail = await resend.emails.send({
+        from: "Rafa Canosa <noreply@rafacanosa.dev>",
+        to: [email], // Envía el correo a la dirección proporcionada
+        subject: "Confirmación de contacto",
+        html: `<h2>Hola${name}</h2><p>Gracias por contactar con nosotros. Le responderemos lo antes posible.</p>`,
+      })
+
+      if (confirmationEmail.error) {
+        throw new ActionError({
+          code: "BAD_REQUEST",
+          message: confirmationEmail.error.message,
         })
       }
 
